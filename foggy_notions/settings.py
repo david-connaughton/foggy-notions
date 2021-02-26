117d081a027da022a9dab014fc553efa12d11432
david connaughton <davidconnaughton@davids-MacBook-Pro.local>
2021-02-26 22:21:00 +0000

    Added Base Templates

--- foggy_notions/settings.py
+++ foggy_notions/settings.py
@@ -59,7 +59,8 @@ ROOT_URLCONF = 'foggy_notions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
