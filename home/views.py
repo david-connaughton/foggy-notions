cd4fa2ff8bc6a10881abcdfb1d6c1915ccd6b447
david connaughton <davidconnaughton@davids-MacBook-Pro.local>
2021-03-01 20:09:06 +0000

    Updated Home App Views

--- home/views.py
+++ home/views.py
@@ -1,7 +1,62 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.contrib import messages
 
-# Create your views here.
+from .models import Post
+
+from django.conf import settings
+from mailchimp_marketing import Client
+from mailchimp_marketing.api_client import ApiClientError
+
+import os
+
+if os.path.exists("env.py"):
+    import env
+
+# MailChimp Settings
+api_key = os.environ.get('MAILCHIMP_API_KEY')
+server = os.environ.get('MAILCHIMP_DATA_CENTER')
+list_id = os.environ.get('MAILCHIMP_EMAIL_LIST_ID')
+
+
+def subscribe(email):
+    """
+    Contains code handling the communication to the mailchimp api
+    to create a contact/member in an audience/list.
+    """
+
+    mailchimp = Client()
+    mailchimp.set_config({
+        "api_key": api_key,
+        "server": server,
+    })
+
+    member_info = {
+        "email_address": email,
+        "status": "subscribed",
+    }
+
+    try:
+        response = mailchimp.lists.add_list_member(list_id, member_info)
+        print("response: {}".format(response))
+    except ApiClientError as error:
+        print("An exception occurred: {}".format(error.text))
 
 
 def home(request):
-    return render(request, 'home/index.html')
+    posts = Post.objects.all()
+    template = 'home/index.html'
+    context = {
+        'posts': posts,
+    }
+    if request.method == 'POST':
+        email = request.POST['email']
+        subscribe(email)   # function to access mailchimp
+        messages.success(request, "Thank you for subscribing!")  # message
+        posts = Post.objects.all()
+        template = 'home/index.html'
+        context = {
+            'posts': posts,
+        }
+        return render(request, "home/index.html", context)
+    return render(request, template, context)
