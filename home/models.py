fad5944cf518a7da7c5ee1fa3345555a7209f2a6
david connaughton <davidconnaughton@davids-MacBook-Pro.local>
2021-03-01 20:00:00 +0000

    Added Post Model to Home App

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date_posted', ]
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
