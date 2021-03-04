761612d9500d93bb0ef03b58ed9203449eed324c
david connaughton <davidconnaughton@davids-MacBook-Pro.local>
2021-03-04 02:06:15 +0000

    Jquery & CSS updated for Mail Button

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $(".cover-text").css("display", "none");
     $(".cover-text").fadeIn(2000);
+    $("#site-button").mouseover(function(){
+        $("#site-button").css("color", '#141411'); 
+    });
+    $("#site-button").mouseout(function(){
+        $("#site-button").css("color", '#ED0000'); 
+    });
     $("#form").validate(); 
 })
